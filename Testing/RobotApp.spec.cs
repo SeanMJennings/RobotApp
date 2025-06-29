c14c8ca7cb373a2586d643e1052ae37870ae5079
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2025-06-29 21:50:13 +0100

    refactor

--- Testing/RobotApp.spec.cs
+++ Testing/RobotApp.spec.cs
@@ -5,42 +5,56 @@ public static partial class RobotAppShould
     [Test]
     public static void RejectNonExistentFile()
     {
-        Given(a_non_existent_file).WhenTransforming(looking_for_file).Then(file_is_not_found);
+        Given(a_non_existent_file)
+            .WhenTransforming(looking_for_file)
+            .Then(file_is_not_found);
     }
     
     [Test]
     public static void AcceptExistingFile()
     {
-        Given(a_known_file).WhenTransforming(looking_for_file).Then(file_is_found);
+        Given(a_known_file)
+            .WhenTransforming(looking_for_file)
+            .Then(file_is_found);
     }
     
     [Test]
     public static void RejectInvalidGridDimensions()
     {
-        Given(a_known_file_with_invalid_grid_size_has_been_read).WhenTransforming(parsing).Then(grid_size_is_invalid);
+        Given(a_known_file_with_invalid_grid_size_has_been_read)
+            .WhenTransforming(parsing)
+            .Then(grid_size_is_invalid);
     }
     
     [Test]
     public static void ParseGridDimensions()
     {
-        Given(a_known_file_has_been_read).WhenTransforming(parsing).Then(grid_size_is_correct);
+        Given(a_known_file_has_been_read)
+            .WhenTransforming(parsing)
+            .Then(grid_size_is_correct);
     }
     
     [Test]
     public static void RejectInvalidStartingLocation()
     {
-        Given(a_known_file_with_invalid_starting_location_has_been_read).WhenTransforming(parsing).Then(starting_location_is_invalid);
+        Given(a_known_file_with_invalid_starting_location_has_been_read)
+            .WhenTransforming(parsing)
+            .Then(starting_location_is_invalid);
     }
     
     [Test]
     public static void RejectInvalidMovements()
     {
-        Given(a_known_file_with_invalid_movements_has_been_read).WhenTransforming(parsing).Then(movements_are_invalid);
+        Given(a_known_file_with_invalid_movements_has_been_read)
+            .WhenTransforming(parsing)
+            .Then(movements_are_invalid);
     }
     
     [Test]
     public static void RejectInvalidEndingLocation()
     {
-        Given(a_known_file_with_invalid_ending_location_has_been_read).WhenTransforming(parsing).Then(ending_location_is_invalid);
+        Given(a_known_file_with_invalid_ending_location_has_been_read)
+            .WhenTransforming(parsing)
+            .Then(ending_location_is_invalid);
     }
 }
